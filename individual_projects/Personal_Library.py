113993ccd75aca161cb113a7b000ecacee4498f8
Zuzanune <Zane.carter@UCAS-edu.net>
2026-02-17 18:47:26 +0000

    did a LOT of stuff that fixed a little tiny errorl

--- individual_projects/Personal_Library.py
+++ individual_projects/Personal_Library.py
@@ -18,8 +18,8 @@ def validate_input(text, kind='int'):
     else:
         return False
 import csv
-with open('individual_projects/Library.csv', 'r') as book:
-    library_reader = csv.writer()
+with open('individual_projects/Library.csv', 'r') as library_file:
+    library = list(csv.DictReader(library_file))
 
 #introduce the program
 print("Welcome to your personal library!")
@@ -28,13 +28,14 @@ def add():
     title = input("Enter book title: ").capitalize()
     author = input("Enter book author: ").capitalize()
     genre = input("enter book genre:  ").capitalize()
-    while "true":
-        year = int(input("enter the books release year:  "))
-        if not validate_input(year):
+    while True:
+        year = input("enter year book was released:  ").strip()
+        if not validate_input(year, kind='int'):
             print ("please enter an intiger value")
             continue
+        year = int(year)
         break
-    for book in library_reader:
+    for book in library:
         if book['title'].lower() == title.lower() and book['author'].lower() == author.lower():
             print("This book is already in the library.")
             doub = input("are you sure you want to add it again? (yes/no)   ").strip().lower()
@@ -64,8 +65,8 @@ def search():
     search_term = input("Enter search term: ").strip().lower()
     #   search list by going through each name in a for loop, then checking if the search term is in the name
     matches = []
-    for book in library_reader:
-        if (search_type == 'title' or search_type == 't') and search_term in book['title'].lower() or (search_type == 'author' or search_type == 'a') and search_term in book['author'].lower() and (search_type == 'genre' or search_type == 'g') and search_term in book['genre'].lower() and (search_type == 'year' or search_type == 'y') and search_term in book['year'].lower():
+    for book in library:
+        if search_type == 'title' and search_term in book['title'].lower() or (search_type == 'author') and search_term in book['author'].lower() or (search_type == 'genre') and search_term in book['genre'].lower() or (search_type == 'year') and search_term in book['year'].lower():
             matches.append(book)
 #   display matching books from library list
     if matches:
@@ -80,51 +81,42 @@ def remove():
     count = 0
     #   ask user for book title to remove
     title_to_remove = input("Enter the title of the book to remove: ").strip().lower()
-    #   remove book from library list
+    #   remove book from library.csv
     matches = []
-    for book in library_reader:
+    for book in library:
         if book['title'].lower() == title_to_remove:
             count += 1
             matches.append(book)
     if count == 0:
-        print ("no book found matching that title")
-    if count > 1:
-        print ("more than one book found matching that title")
-        for x in matches:
-            print (f'"{x["title"]}" by {x["author"]}')
-        author_to_remove = input("Enter the author of the book to remove: ").strip().lower()
-        for book in library_reader:
-            if book['title'].lower() == title_to_remove and book['author'].lower() == author_to_remove:
-                with open('individual_projects/Library.csv', 'r') as books:
-                    lines = books.readlines()
-                with open('individual_projects/Library.csv', 'w') as books:
-                    for line in lines:
-                        if not (line.strip().split(',')[0].lower() == title_to_remove and line.strip().split(',')[1].lower() == author_to_remove):
-                            books.write(line)
-                print(f'Book "{book["title"]}" by {book["author"]} removed from library.')
-                return
-            print ("no book matches that title and author")
-    if count == 1:
-        with open('individual_projects/Library.csv', 'r') as books:
-            lines = books.readlines()
-        with open('individual_projects/Library.csv', 'w') as books:
-            for line in lines:
-                if not (line.strip().split(',')[0].lower() == title_to_remove and line.strip().split(',')[1].lower() == author_to_remove):
-                    books.write(line)
-        print(f'Book "{book["title"]}" by {book["author"]} removed from library.')
+        print("no book found matching that title")
         return
+    if count > 1:
+        print("more than one book found matching that title")
+        for x in matches:
+            print(f'"{x["title"]}" by {x["author"]}')
+        author_to_remove = input("Enter the author of the book to remove: ").strip().lower()
+    else:
+        author_to_remove = matches[0]['author'].lower()
+    with open('individual_projects/Library.csv', 'r') as books:
+        lines = books.readlines()
+    with open('individual_projects/Library.csv', 'w') as books:
+        for line in lines:
+            if not (line.strip().split(',')[0].lower() == title_to_remove and line.strip().split(',')[1].lower() == author_to_remove):
+                books.write(line)
+    print(f'Book "{title_to_remove}" by {author_to_remove} removed from library.')
 
 #define view function
 def view():
     #   display all books in library list
-    if library_reader:
+    emp = False
+    if library:
         print("Books in library:")
-        for book in library_reader:
-            print(f'"{book["title"]}" by {book["author"]}')
+        for book in library:
+            print(f'{book["title"]} by {book["author"]}')
+        emp = True
     else:
         print("Library is empty.")
-        emp = True
-    if not emp:
+    if emp:
         while True:
             while True:
                 select = input("would you like to select any of these books? (y/n) \n").lower()
@@ -132,18 +124,21 @@ def view():
                     print ("please enter y or n")
                     continue
                 break
-            if select == "y" or "yes":
+            if select == "y" or select =="yes":
                 name = input("please enter the name of the book you are looking for:  \n")
-                for book in library_reader:
+                for book in library:
                     if book["title"] == name:
                         print (f"title: {book['title']} \n author: {book['author']} \n genre : {book['genre']} \n year released: {book['year']}")
-            if select == 'n' or 'no':
+            if select == 'n' or select == 'no':
                 break
     return
 
 #define main function
 def main():
+    global library
     while True:
+        with open('individual_projects/Library.csv', 'r') as library_file:
+            library = list(csv.DictReader(library_file))
     #   display menu with options to add, search, remove, or quit
         print ("would you like to add, remove, search, view, or quit")
         menu_choice = input()
